a4c96181b354cab8fff5495128e98e13b9697f1f
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2025-09-08 17:20:47 +0100

    going back to plain css and min tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,6 @@ module.exports = {
         primary: ["Roboto", "sans-serif"],
         accent: ["Marcellus", "serif"],
       },
-      // fontSize: {
-      //   "h1-clamp": [
-      //     "clamp(2.5rem, 3rem + 2vw, 4rem)",
-      //     { lineHeight: "1.2", letterSpacing: "0.2rem" },
-      //   ],
-      //   "subtitle-clamp": [
-      //     "clamp(1rem, 1.2rem + 1vw, 1.5rem)",
-      //     { lineHeight: "1.2" },
-      //   ],
-      // },
       maxWidth: {
         container: "1240px",
       },
