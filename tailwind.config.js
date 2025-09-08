94682744bc8343821e95902d695243b2e00b97f7
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2025-09-08 15:10:37 +0100

    hero section styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,22 @@ module.exports = {
         primary: "#1b232e",
         secondary: "#685769",
         gray: "#87839a",
+        light: "#f5f5f5",
       },
       fontFamily: {
         primary: ["Roboto", "sans-serif"],
         accent: ["Marcellus", "serif"],
       },
+      // fontSize: {
+      //   "h1-clamp": [
+      //     "clamp(2.5rem, 3rem + 2vw, 4rem)",
+      //     { lineHeight: "1.2", letterSpacing: "0.2rem" },
+      //   ],
+      //   "subtitle-clamp": [
+      //     "clamp(1rem, 1.2rem + 1vw, 1.5rem)",
+      //     { lineHeight: "1.2" },
+      //   ],
+      // },
       maxWidth: {
         container: "1240px",
       },
